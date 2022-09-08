1ab5faabb1ac3879f2933d26fa4a189d2b6a9a1f
Agustin Grosso <agustigrosso26@gmail.com>
2022-09-07 23:10:45 -0300

    ModificacionAlert

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@ function addToCart(id){
 function removeFromCart(id){
    
       Swal.fire({
-            title: 'Está seguro de eliminar el producto?',
+            title: '¿Está seguro de eliminar el producto?',
             icon: 'warning',
             background: '#000',
             color:'#ffff',
@@ -111,12 +111,12 @@ function removeFromCart(id){
     
             if (result.isConfirmed) {
                 Swal.fire({
-                    title: 'Borrado!',
+                    title: '¡Eliminado!',
                     icon: 'success',
                     background: '#000',
                     color:'#ffff',
                     confirmButtonColor: '#17301C',
-                    text: 'El archivo ha sido borrado'
+                    text: 'El producto se ha eliminado'
                     
                 })
                 carrito.splice(id,1);
@@ -130,71 +130,4 @@ function almacenarEnStorage(){
 }
 
 
-
-
-
 renderLibros();
-
-
-
-
-// ARRAY DE LIBROS
-// let books = [
-//     {id:0, name:"nunca llegamos a las indias", price:2500, category: "ficcion", img:'img/juanS.png'},
-//     {id:1, name:"en el camino", price:2000, category: "ficcion", img:'img/enElCamino.webp'},
-//     {id:2, name:"el tunel", price:3000, category: "ficcion", img:'img/tunelS.jpg'},
-// ];
-
-// let cart = [];
-
-// function renderBooks(){
-//     let html = "";
-
-//     for(let i = 0; i < books.length; i++){
-//         html = html + `
-//         <div onclick ="addToCart(${books[i].id});" style="border: 1px solid green; margin: 10px;">
-//             <img src="${books[i].img}" alt="">
-//             <p>id: ${books[i].id}</p>
-//             <p>nombre: ${books[i].name}</p>
-//             <p>Precio: ${books[i].price}</p>
-//             <p>Categoria: ${books[i].category}</p>
-//         </div>
-//         `;
-//     }
-//     document.getElementById("div-products").innerHTML = html
-// }
-// // FUNCION CUANDO AGREGO O MUESTO EN/AL CARRITO
-// function renderCart(){
-
-//     let html = "";
-
-//     for(let i = 0; i < cart.length; i++){
-//         html = html + `
-//         <div style="border: 1px solid green; margin: 10px;">
-//             <p>id: ${cart[i].id}</p>
-//             <p>nombre: ${cart[i].name}</p>
-//             <p>Precio: ${cart[i].price}</p>
-//             <p>Categoria: ${cart[i].category}</p>
-//             <span style="cursor:pointer"; onclick="removeFromCart(${i});">BORRAR</span>;
-//         </div>
-//         `;
-//     }
-//     document.getElementById("div-cart").innerHTML = html
-// }
-
-// function addToCart(id){
-//     const foundBooks = books.find(item => item.id == id);
-    
-//     cart.push(foundBooks);
-//     renderCart();
-// }
-
-
-// function removeFromCart(id){
-//     cart.splice(id,1);
-//     renderCart();
-// }
-
-
-
-// renderBooks();
